4a24a2171c2f00dbd6f4d5063d470ecb7d397dcf
Svetlana <Svetlanat1005@yandex.ru>
2024-10-29 15:23:49 +0300

    add BottleOfWater

---
+++ BottleOfWater.java
@@ -0,0 +1,28 @@
+public class BottleOfWater extends Product{
+
+    private int volume;
+
+    // public BottleOfWater(String name, int price) {
+    //     super(name, price);
+           
+    // }
+
+    public BottleOfWater(String name, int price, int volume) {
+        super(name, price);
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    @Override
+    public String toString() {
+        return "BottleOfWater [name=" + getName() + ", price=" + getPrice() + ", volume=" + volume + "]";
+    }
+    
+}
