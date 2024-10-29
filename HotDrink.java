bf3b7ec057139ff4fcafbd305003e12f0cb19e86
Svetlana <Svetlanat1005@yandex.ru>
2024-10-29 15:27:13 +0300

    add HotDrink

---
+++ HotDrink.java
@@ -0,0 +1,31 @@
+public class HotDrink extends Product{
+
+    private int temperature;
+
+    public HotDrink(String name, int price) {
+        super(name, price);
+    }
+
+    public HotDrink(String name, int price, int temperature) {
+        super(name, price);
+        this.temperature = temperature;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    @Override
+    public String toString() {
+        return "HotDrink [name=" + getName() + ", price=" + getPrice() + ", temperature=" + getTemperature() + "]";
+    }
+
+    
+
+
+
+}
